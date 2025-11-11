82c65d343db2a681628b1f80fbc672c5e8994f95
Anthony Arnold <stubbiegaming@gmail.com>
2025-11-11 07:49:28 -0500

    rbt complete impl

--- rbt/rbt_runner.sh
+++ rbt/rbt_runner.sh
@@ -5,12 +5,19 @@ cwd=$(pwd)
 bosque=$cwd/../../BosqueCore
 cpp=$bosque/bin/src/cmd/analyzecpp.js
 
+build=dev
 cppout=$cwd/cppout
 src=$1
+
+common=$cppout/gc/src/runtime/common.h
 
 if [[ $# -eq 0 ]]; then
     echo "No source file provided!"
     exit 1
+fi
+
+if [[ $2 == "release" ]]; then 
+    build=release
 fi
 
 function cleanup() {
@@ -24,7 +31,17 @@ cleanup
 
 node $cpp $src
 
+# Setup to be running with asserts off and packed metadata
+# FS is input field separator (so nothing here), RS defines a line
+# (record field separator)
+commontmp=$(mktemp)
+awk -v rmv="//#define BSQ_GC_CHECK_ENABLED\n//#define VERBOSE_HEADER" 'BEGIN { RS = ""; FS = "" } {
+    gsub(/#define BSQ_GC_CHECK_ENABLED\n#define VERBOSE_HEADER/, rmv)
+    print
+}' "$common" > "$commontmp"
+mv "$commontmp" "$common"
+
 cd $cppout
 make clean
-make
+make BUILD=$build
 ./output/memex
